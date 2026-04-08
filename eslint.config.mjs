9db1a39e4e3c2c2b33633dd4aeb012b48565bbc2
YutaCode <ytknknorm820@gmail.com>
2026-04-08 17:19:24 +0900

    feat: complete Chapter 13

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,10 +4,9 @@ import nextVitals from 'eslint-config-next/core-web-vitals';
 const eslintConfig = defineConfig([
   ...nextVitals,
   {
-    // ここで設定を追加して、自動検出をスキップさせます
     settings: {
       react: {
-        version: "19.0" // または "18.0"。Next.js 15以上なら19でOKです
+        version: "19.0"
       }
     }
   },
